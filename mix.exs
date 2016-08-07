91030bca36a923c28b13fd612963c59336f0f785
Gerard de Brieder <smeevil@me.com>
2016-08-07 15:43:12 +0200

    updated mix file

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule OpenExchangeRates.Mixfile do
   def project do
     [
       app: :open_exchange_rates,
+      description: "A library that can convert currencies and uses automatic updating of the currrency rates using openexchangerates.org",
+      package: package,
       version: "0.1.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
@@ -31,6 +33,18 @@ defmodule OpenExchangeRates.Mixfile do
       {:mix_test_watch, ">0.0.0", only: [:dev, :test]},
       {:ex_doc, ">0.0.0", only: [:dev, :test]},
       {:excoveralls, "~> 0.5", only: :test},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Gerard de Brieder"],
+      licenses: ["MIT"],
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*", "CHANGELOG*"],
+      links: %{
+        "GitHub" => "https://github.com/smeevil/open_exchange_rates",
+        "Docs"   => "https://smeevil.github.com/open_exchange_rates"
+      }
     ]
   end
 end
